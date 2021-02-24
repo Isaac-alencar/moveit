84c614f809f047936ce86b66dc242fa90957a56a
Isaac alencar <alencarisaac3@gmail.com>
2021-02-23 22:28:29 -0300

    fix:remove title from _document
    Fixing bug with title tag, folowing the best pratice recomended by Next docs, removing title tag from _document and put it on _app

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,6 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="shortcut icon" href="favicon.png" type="image/png"/>
-          <title>Home | move.it</title>
         </Head>
         <body>
           <Main />
